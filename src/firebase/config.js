1f1d1018a1d8f00bebd5dc4a01387f0f31cc2054
Vinamra Sareen <vsareen24@gmail.com>
2020-11-11 01:43:55 +0530

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCERULtAcuc4x6WygWOJZnnNUJCNnXe9gw",
+  authDomain: "hunt-99572.firebaseapp.com",
+  databaseURL: "https://hunt-99572.firebaseio.com",
+  projectId: "hunt-99572",
+  storageBucket: "hunt-99572.appspot.com",
+  messagingSenderId: "586131759646",
+  appId: "1:586131759646:web:dc5949546fee5815a8c40f",
+};
+
+export default firebaseConfig;
